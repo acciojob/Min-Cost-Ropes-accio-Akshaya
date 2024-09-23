2db49a1e538ec6b9fdbb30c892a861897dca511d
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-09-22 23:55:16 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function mincost(arr)
 { 
 	let totalCost = 0;
-  const minHeap = new MinHeap();
 
-  
-  arr.forEach(length => minHeap.insert(length));
+  // Step 1: Initialize a min-heap (priority queue) and add all ropes to it
+  const minHeap = new MinHeap((a, b) => a - b);  // min heap (smallest element on top)
+  arr.forEach(length => minHeap.push(length));
 
-  
+  // Step 2: While there is more than one rope, extract two smallest ropes
   while (minHeap.size() > 1) {
-    const first = minHeap.extractMin();
-    const second = minHeap.extractMin();
-    const cost = first + second;
-    totalCost += cost;
-    minHeap.insert(cost);
+    const first = minHeap.pop(); 
+    const second = minHeap.pop(); // Second smallest rope
+
+    const cost = first + second;  // Cost of connecting the two ropes
+    totalCost += cost;            // Add cost to total
+
+    minHeap.push(cost);  
 }
 
 module.exports=mincost;
