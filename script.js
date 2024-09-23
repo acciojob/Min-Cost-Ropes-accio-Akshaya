093c8563ab6bab6d3d1d490f126a14ef6798b555
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-09-22 23:34:43 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function mincost(arr)
 { 
+	if (arr.length <=1) 
+		return 0;
+	arr.sort((a,b)=>a-b);
+	let totalcost=0;
+	while (arr.length > 1) {
+		let first = arr.shift();
+		let second = arr.shift();
+
+		let cost = first + second;
+		totalcost += cost;
+
+		arr.push(cost);
+		arr.sort((a,b)=>a-b);
+		
+	}
 //write your code here
+	return totalCost;
 // return the min cost
   
 }
